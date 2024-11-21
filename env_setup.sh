8c013b5ad2bd785860d89a4182ae4b6f3f92d82d
focusunsink <549303468@qq.com>
2024-11-21 16:37:37 +0800

    Create env_setup.sh

---
+++ env_setup.sh
@@ -0,0 +1,4 @@
+pip3 install vllm==0.6.3 -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+# if you need fp8
+#git clone https://github.com/neuralmagic/AutoFP8.git
+#pip3 install -e AutoFP8/ --force-reinstall torch==2.4.0 -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com 
