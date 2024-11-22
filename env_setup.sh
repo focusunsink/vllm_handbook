625291b28018c2adfe7bc1c60592a36c202aeaa7
focusunsink <549303468@qq.com>
2024-11-22 11:18:21 +0800

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -1,8 +1,12 @@
-pip3 install vllm==0.6.3 -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
-# if you need fp8
+
+# 1. if you need fp8
 #git clone https://github.com/neuralmagic/AutoFP8.git
 #pip3 install -e AutoFP8/ --force-reinstall torch==2.4.0 -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com 
 
-# if you need w8a8
+# 2. if you need w8a8
 # to prevent this issue https://github.com/vllm-project/llm-compressor/issues/910
 pip install compressed-tensors==0.7.1
+
+# 3. Flashinfer
+# https://flashinfer.ai/whl/cu124/torch2.4/flashinfer/
+pip install --no-cache-dir https://github.com/flashinfer-ai/flashinfer/releases/download/v0.1.6/flashinfer-0.1.6%2Bcu124torch2.4-cp38-cp38-linux_x86_64.whl
