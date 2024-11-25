d2f6223470a3942aa26e0a5c119e39c9196b6ae5
focusunsink <549303468@qq.com>
2024-11-25 20:08:19 +0800

    Create run.sh

---
+++ example/model/qwen2.5/3b/bf16/run.sh
@@ -0,0 +1,7 @@
+# H800
+# Processed prompts: 100% 1/1 [00:00<00:00,  2.70it/s, est. speed input: 16187.68 toks/s, output: 140.29 toks/s]
+# time used :  0.3717203140258789 avg used 0.007148467577420748
+
+nsys profile \
+--stats=true -o qwen2.5_3b_bf16.nsys-rep -ftrue -t cuda,nvtx --delay=5 \
+python3 demo.py ../Qwen2.5-3B/ 6000
